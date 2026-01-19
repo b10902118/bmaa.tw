8238c8940018ced5ce5f8236f709907cba66125e
Bill Tsui <b10902118@ntu.edu.tw>
2026-01-20 00:50:10 +0800

    adjust header footer max width

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <footer className="bg-[#6B705C] text-white">
-      <div className="max-w-[1140px] mx-auto px-4 py-16">
-        <div className="max-w-4xl mx-auto space-y-8 mb-12">
+      <div className="max-w-[1140px] 2xl:max-w-[66%] mx-auto px-4 py-16">
+        <div className="mx-auto space-y-8 mb-12">
           <h3 className="text-2xl font-bold">關於我們……</h3>
           <p className="text-[20px] leading-[1.8] font-light">
             我們是一群因練習身心中軸覺察而獲益良多的心理學研究者與相關專業人士。從人文關懷出發，以分享好東西的熱情以及科學探索的精神，協助現代人覺察身體與認識自己，啟動改變身心的力量，找回專注力與幸福感。我們相信，每個人都能獲得身心的平靜與安穩，進而實踐自我、與世界共好。
